5b937b959d68dd667cbdc32b02277e7a2d1c0316
huhuanming <workboring@gmail.com>
2016-01-14 12:10:45 +0800

    remove url safe

--- signature.java
+++ signature.java
@@ -23,7 +23,7 @@ class MeiqiaToolsSigner {
             mac.init(signingKey);
             byte[] rawHmac = mac.doFinal(raw_body.getBytes("UTF-8"));
             byte[] hexBytes = new org.apache.commons.codec.binary.Hex().encode(rawHmac);
-            result = org.apache.commons.codec.binary.Base64.encodeBase64URLSafeString(hexBytes).trim();
+            result = org.apache.commons.codec.binary.Base64.encodeBase64String(hexBytes).trim();
         } catch (Exception e) {
             throw new SignatureException("Failed to generate HMAC : " + e.getMessage());
         }
