4a18d34bb3984ac70657731e9d8d0fe0e5c8c11f
hu <workboring@gmail.com>
2016-09-13 17:42:17 +0800

    add go test

---
+++ Golang/signature_test.go
@@ -0,0 +1,35 @@
+package meiqia
+
+import (
+	"testing"
+	"io/ioutil"
+    "encoding/json"
+)
+
+type jsonType struct {
+    Key string
+    Maps  []mapType
+}
+
+type mapType struct {
+	Text string
+	Sign string
+}
+
+func TestMSigner(t *testing.T) {
+	sample_text, err := ioutil.ReadFile("../sample.txt")
+    if err != nil {
+        panic(err)
+    }
+
+    var sample_json jsonType 
+    json.Unmarshal(sample_text, &sample_json)
+
+    for _, _map := range sample_json.Maps {
+    	expected := _map.Sign
+    	actual := MSigner(_map.Text, sample_json.Key)
+    	if actual != expected {
+			t.Error("text: %s, sign: %s", _map.Text, _map.Sign)
+		}
+    }
+}
